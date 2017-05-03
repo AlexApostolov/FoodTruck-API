c8f86589ba4389425a1b7c421e636e6a3697209a
Alexander Apostolov <emailme@alexanderapostolov.com>
2017-05-03 19:17:24 -0400

    Added review model and some refactoring

--- src/controller/foodtruck.js
+++ src/controller/foodtruck.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import {Router} from 'express';
 import FoodTruck from '../model/foodtruck';
+import Review from '../model/review';
 
 export default({config, db}) => {
   let api = Router();
@@ -10,12 +11,44 @@ export default({config, db}) => {
   api.post('/add', (req, res) => {
     let newFoodTruck = new FoodTruck();
     newFoodTruck.name = req.body.name;
+    newFoodTruck.foodtype = req.body.foodtype;
+    newFoodTruck.avgcost = req.body.avgcost;
+    newFoodTruck.geometry.coordinates = req.body.geometry.coordinates;
 
     newFoodTruck.save(err => {
       if (err) {
         res.send(err);
       }
       res.json({message: 'FoodTruck saved successfully'});
+    });
+  });
+
+  // add review for a specific foodtruck id
+  // '/v1/foodtruck/reviews/add/:id'
+  api.post('/reviews/add/:id', (req, res) => {
+    FoodTruck.findById(req.params.id, (err, foodtruck) => {
+      if(err) {
+        res.send(err);
+      }
+      let newReview = new Review();
+
+      newReview.title = req.body.title;
+      newReview.text = req.body.text;
+      newReview.foodtruck = foodtruck._id;
+      newReview.save((err, review) => {
+        if(err) {
+          res.send(err);
+        }
+        // push newReview object to "reviews" array reference to model
+        foodtruck.reviews.push(newReview);
+        // and then save it
+        foodtruck.save(err => {
+          if(err) {
+            res.send(err);
+          }
+          res.json({message: 'Food truck review saved!'});
+        });
+      });
     });
   });
 
@@ -40,8 +73,19 @@ export default({config, db}) => {
     });
   });
 
+  // get reviews for specific food truck id '/v1/foodtruck/reviews/:id'
+  api.get('/reviews/:id', (req, res) => {
+    Review.find({foodtruck: req.params.id}, (err, reviews) => {
+      if(err) {
+        res.send(err);
+      }
+      res.json(reviews);
+    });
+  });
+
+
   // UPDATE
-  // update existing foodtruck '/vi1/foodtruck/:id'
+  // update existing foodtruck '/v1/foodtruck/:id'
   api.put('/:id', (req, res) => {
     FoodTruck.findById(req.params.id, (err,foodtruck) => {
       if (err) {
